910faf4e43454ad9ec7e1a1c430fa7e52727cf82
Sodahiya <Sodahiya@gmail.com>
2024-10-17 11:17:36 +0530

    Added new Scripts

--- Class/twentytwo.c
+++ Class/twentytwo.c
@@ -25,15 +25,16 @@ int main(){
   char symbols[] = "!@#$^&*?";
 
   srand(time(NULL));
-  char pass[8];
+  char pass[10];
   pass[0] = LETTER[rand()%26];
   pass[1] = numbers[rand()%10];
   pass[2] = numbers[rand()%26];
   pass[3] = symbols[rand()%8];
 
-  for(int i = 4;i<8;i++){
+  for(int i =4;i<8;i++){
     pass[i] = letter[rand()%26];
   }
+
 
   printf("Password is: %s",pass);
 
